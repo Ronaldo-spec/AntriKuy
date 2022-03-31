bbadbcc957b319fe965e1cd07099c2909acba137
Aula-Faridah <aulafaridah@gmail.com>
2022-03-31 19:57:01 +0700

    add master blade

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,13 @@
+@extends ('layouts.master')
+
+@section('content')   
+    <!-- Landing Page Start -->
+    <div class="container-fluid pt-4 px-4">
+        <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
+            <div class="col-md-6 text-center">
+                <h3>This is landing page</h3>
+            </div>
+        </div>
+    </div>
+    <!-- Landing Page End -->
+@endsection
