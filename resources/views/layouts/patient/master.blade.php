227ebeed73c04de17e31de07a30a35ee46958c92
Ronaldo Firmansyah <ronaldofirmansyah.30@gmail.com>
2022-05-27 14:49:32 +0700

    Revert "maap salah"
    This reverts commit 09309110b5f41373482fa7f651fbbc1fff48ed28.

--- resources/views/layouts/patient/master.blade.php
+++ resources/views/layouts/patient/master.blade.php
@@ -1,129 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.base')
 
-<head>
-    <meta charset="utf-8">
-    <title>Rs Tadika Mesra - Halaman Pasienimage.png</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+@section('body')
+<div id="site-content">
+	<div class="site-header">
+		<div class="container">
+			<a href="{{ url('/') }}" id="branding">
+				<img src="images/logo.png"  class="logo">
+				<div class="logo-text">
+					<h1 class="site-title" >RS Tadika Mesra</h1>
+					<small class="site-description">Untuk Pelayanan yang Lebih Baik</small>
+				</div>
+			</a> <!-- #branding -->
 
-    <!-- Favicon -->
-    <link href="{{asset('img/favicon.ico')}}" rel=" icon">
+			<!-- Default snippet for navigation -->
+			<div class="main-navigation">
+				<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+				<ul class="menu">
+					<li class="menu-item"><a href="{{route('landing')}}">Home</a></li>
+					<li class="menu-item"><a href="about.html">About</a></li>
 
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-    <link href="{{asset('lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css')}}" rel="stylesheet" />
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{asset('css/bootstrapp.min.css')}}" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
-    <link href="{{asset('css/stylep.css')}}" rel="stylesheet">
-</head>
-
-<body>
-    <div class="container-fluid position-relative bg-white d-flex p-0">
-        <!-- Sidebar Start -->
-        <div class="sidebar pe-4 pb-3">
-            <nav class="navbar bg-light navbar-light">
-                <a href="index.html" class="navbar-brand mx-4 mb-3">
-                    <h3 class="text-primary"><i class="fa fa-hospital me-2"></i></i>RSTM</h3>
-                </a>
-                <div class="d-flex align-items-center ms-4 mb-4">
-                    <div class="position-relative">
-                        <img class="rounded-circle" src="{{asset('images/user.jpg')}}" alt="" style=" width: 40px; height: 40px;">
-                        <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
-                        </div>
-                    </div>
-                    <div class="ms-3">
-                        <h6 class="mb-0">{{ Auth::user()->username }}</h6>
-                        <span>{{ Auth::user()->role }}</span>
-                    </div>
-                </div>
-                <div class="navbar-nav w-100">
-                    <a href="{{route('pasien.index')}}" class="nav-item nav-link @yield('statussidepanel1')"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-                    <a href="{{route('users.index')}}" class="nav-item nav-link @yield('statussidepanel2')"><i class="fa fa-user me-2"></i>Manage Users</a>
-                    <a href="#" class="nav-item nav-link @yield('statussidepanel5')"><i class="fa fa-clipboard-list me-2"></i>Antrian</a>
-                </div>
-            </nav>
-        </div>
-        <!-- Sidebar End -->
-
-
-        <!-- Content Start -->
-        <div class="content">
-            <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
-                <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
-                    <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
-                </a>
-                <a href="#" class="sidebar-toggler flex-shrink-0">
-                    <i class="fa fa-bars"></i>
-                </a>
-                <div class="d-none d-md-flex ms-4">
-                    <a href="{{ route('home') }}">Home</a>
-                </div>
-                <div class="navbar-nav align-items-center ms-auto">
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="{{asset('images/user.jpg')}}" alt="" style="width: 40px; height: 40px;">
-                            <span class="d-none d-lg-inline-flex">{{ Auth::user()->username }}</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">My Profile</a>
-                            <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
+					@if(Auth::user())
+					<li class="menu-item">
+						<a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-            <!-- Navbar End -->
-            @yield('content')
-            <!-- Footer Start -->
-            <br>
-            <div class="container-fluid pt-4 px-4">
-                <div class="bg-light rounded-top p-4">
-                    <div class="row">
-                        <div class="col-12 col-sm-6 text-center text-sm-start">
-                            &copy; <a href="#">Rs Tadika Mesra</a>, All Right Reserved.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Footer End -->
-        </div>
-        <!-- Content End -->
-        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+							{{ __('Logout') }}
+						</a>
+					</li>
+					<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+						@csrf
+					</form>
+					@else
+					<li class="menu-item">
+						<a href="{{ route('login') }}">{{ __('Login') }}</a>
+					</li>
+					<li class="menu-item">
+						<a href="{{ route('register') }}">{{ __('register') }}</a>
+					</li>
+					@endif
+				</ul> <!-- .menu -->
+			</div> <!-- .main-navigation -->
 
-    </div>
+			<div class="mobile-navigation"></div>
+		</div>
+	</div> <!-- .site-header -->
 
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{asset('lib/chart/chart.min.js')}}"></script>
-    <script src="{{asset('lib/easing/easing.min.js')}}"></script>
-    <script src="{{asset('lib/waypoints/waypoints.min.js')}}"></script>
-    <script src="{{asset('lib/owlcarousel/owl.carousel.min.js')}}"></script>
-    <script src="{{asset('lib/tempusdominus/js/moment.min.js')}}"></script>
-    <script src="{{asset('lib/tempusdominus/js/moment-timezone.min.js')}}"></script>
-    <script src="{{asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+	@yield('content')
 
-    <!-- Template Javascript -->
-    <script src="{{asset('js/main.js')}}"></script>
-</body>
+	<footer class="site-footer">
+		<div class="container">
+			<div class="pull-left">
 
-</html>
+				<address>
+					<strong>RS Tadika Mesra</strong>
+					<p> <img src="images/instagram.png" width="20" height="20"> @ps_tamesra</p>
+				</address>
+
+
+				<a href="#" class="phone">+ 1 800 931 033</a>
+			</div> <!-- .pull-left -->
+			<div class="pull-right">
+
+				<div class="social-links">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-google-plus"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-pinterest"></i></a>
+
+				</div>
+
+			</div> <!-- .pull-right -->
+
+			<div class="colophon">
+				<img src="images/facebook.png" width="20" height="20">
+				ps.tamesra
+			</div>
+
+		</div> <!-- .container -->
+	</footer> <!-- .site-footer -->
+</div>
+@endsection
